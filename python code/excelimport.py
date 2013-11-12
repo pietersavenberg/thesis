31134675c132a5b98254f3a07a748b077f7187e9
pietersavenberg <Pieter@MacBook-Air-van-Pieter.local>
2013-11-12 18:06:21 +0100

    import plus file save (Belgium+Denmark)
    x en y worden gesaved in bestanden (bevinden zich in Users/Pieter)

--- python code/excelimport.py
+++ python code/excelimport.py
@@ -15,25 +15,27 @@ y = y[1:]  #remove the zeros (first row)
     
 y = y.T #transpose
 
+print y.shape
+
 det = open_workbook('keyind.xlsx')
-sheet = det.sheet_by_index(0)
+belgium = det.sheet_by_index(0)
 
 
 x = np.zeros(1057)
 for j in range(39,101):
     a = []
     for i in range(6,1063):
-        a.append(sheet.cell(i,j).value) #cell(1,0) = tweede rij, eerste kolom
+        a.append(belgium.cell(i,j).value) #cell(1,0) = tweede rij, eerste kolom
         
     x = np.vstack((x,np.array(a)))
      
       
-sheet = det.sheet_by_index(1)
+denmark = det.sheet_by_index(1)
   
 for j in range(56,142):
     a = []
     for i in range(3,1060):
-        a.append(sheet.cell(i,j).value) #cell(1,0) = tweede rij, eerste kolom
+        a.append(denmark.cell(i,j).value) #cell(1,0) = tweede rij, eerste kolom
         
     x = np.vstack((x,np.array(a)))
      
@@ -44,8 +46,9 @@ x = x[1:]  #remove the zeros (first row)
     
 x = x.T #transpose
 
-#print x.shape
+print x.shape
 #print x[0]
 
 
-    
+np.save('y', y)
+np.save('x', x)
