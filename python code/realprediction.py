90ca6a006a99439b5ddc5d3235e11c331a12f88a
pietersavenberg <Pieter@MacBook-Air-van-Pieter.local>
2013-11-20 14:53:06 +0100

    kleine onbelangrijke wijziging

--- python code/realprediction.py
+++ python code/realprediction.py
@@ -11,13 +11,15 @@ def rchange(arr):
     
     
 yolo = np.load('y.npy')
+yoloalsdet = np.load('yalsdet.npy')
 tijdquart = np.load('a.npy') #tijdsas voor kwartaal data: - 92 0 enz.
 tijdmaand = np.load('b.npy') #tijdsas voor maandelijkse data: - 31 0 28 enz.
 qdata = np.load('q.npy') #variable y-as per determinant voor kwartaal data
 mdata = np.load('m.npy') #variable y-as per determinant voor maandelijkse data
 
-
-for j in range(5299,7399,7):
+# t = 0 is 15 feb 1993
+#for j in range(5299,7399,7): #op 7392 = 13 mei 2013 wordt laatste allocatie gedaan en wordt voorspeld wa y is op 7399 adhv x op 7392
+for j in range(5299,5306,7):
     voorspellingsdag =j # 20 augustus 07, voorspellingsdag modulo 7 moet 0 zijn!!
     
     print "voorspellingsdag is ",j 
@@ -62,12 +64,15 @@ for j in range(5299,7399,7):
         
         
     x = x[1:] #eerste rij zeros weg
+    yalsdet = yoloalsdet[:,:voorspellingsdag/7+1]
+    x = np.vstack((x,yalsdet))
     x = x.T  
     
     X = x[:voorspellingsdag/7] #alles behalve de laatste waarde (= de waarde van de det op de voorspellingsdag) wordt gebruikt voor training
     y = yolo[:voorspellingsdag/7]
-    X = np.hstack((X,X))
     
+    #X = np.hstack((X,X))
+    #X = X[:,:1]
     n_features = X.shape[1]
     theta0 = np.ones(2*n_features+3)
     thetaL = theta0 * 1e-1
@@ -78,5 +83,12 @@ for j in range(5299,7399,7):
     
     gp.fit(X, y)
     '''
+    #verwijder de :1 hieronder, is gewoon om te testen
     x = x[voorspellingsdag/7:] #de laatste waarde wordt gebruikt om voorspelling te doen 
-    pred=gp.predict(x)'''
+    pred=gp.predict(x)
+    print("de voorspelling is ", pred)
+    
+    http://scikit-learn.org/stable/modules/model_evaluation.html#scoring-parameter
+    http://scikit-learn.org/stable/modules/grid_search.html
+    
+    '''
